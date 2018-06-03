3d26aefc47afda3651efa681813b9132ed832a8f
yangkai <ykredhat@163.com>
2018-06-03 16:03:24 +0800

    工程初始化

---
+++ src/main/java/com/oh/scada/task/operation/AbstractOperation.java
@@ -0,0 +1,7 @@
+package com.oh.scada.task.operation;
+
+/**
+ * Created by tpusers on 2018/6/3.
+ */
+public class AbstractOperation {
+}
---
+++ src/main/java/com/oh/scada/task/util/TaskUtil.java
@@ -0,0 +1,10 @@
+package com.oh.scada.task.util;
+
+/**
+ * Created by tpusers on 2018/6/3.
+ */
+public class TaskUtil {
+    public static void testUtil(){
+        System.out.println("111");
+    }
+}
