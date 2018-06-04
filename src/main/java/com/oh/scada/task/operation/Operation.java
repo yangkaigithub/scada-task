af7e299e397c4830190871bc5708ad6908704a17
Your Name <you@example.com>
2018-06-04 14:45:10 +0800

    结构搭建

---
+++ src/main/java/com/oh/scada/task/operation/Operation.java
@@ -0,0 +1,15 @@
+package com.oh.scada.task.operation;
+
+/**
+ * Created by tpusers on 2018/6/3.
+ */
+public class Operation {
+
+    public void operate(int vid , int opCode ,int type){}
+
+    public void operate(int vid , int opCode ){}
+
+    public void reOperate(int vid , int opCode){}
+
+
+}
---
+++ src/main/java/com/oh/scada/task/taskManager/TaskManager.java
@@ -0,0 +1,8 @@
+package com.oh.scada.task.taskManager;
+
+/**
+ * Created by wjy on 2018/6/3.
+ */
+public class TaskManager {
+    
+}
