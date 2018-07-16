55bed3cbdc0a1296e7b2aed696f004ce5306ad5f
Your Name <you@example.com>
2018-07-16 14:36:55 +0800

    www

---
+++ src/main/java/com/oh/scada/task/operation/OperationStatus.java
@@ -0,0 +1,9 @@
+package com.oh.scada.task.operation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by wjy on 2018/7/15.
+ */
+public class OperationStatus {
+}
